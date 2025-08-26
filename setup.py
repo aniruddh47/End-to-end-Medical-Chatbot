6b945f2eb5472fbd05785871383e403e5ee94381
aniruddh47 <aniruddh4722@gmail.com>
2025-08-26 15:02:32 +0530

    folder structure edit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aniruddh Kumbhar',
+    author_email= 'aniruddh4722@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
